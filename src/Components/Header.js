dc301c47a347d00751c6b5c7d935b7c050193d09
Muskan <mishra2001muskan@gmail.com>
2022-11-04 17:09:16 +0530

    navBar added

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+
+const Header=()=>{
+    return(
+<>
+    <Container fluid className="header">
+        <h1 style={{background:"red",width:100,height:"10%",boxShadow:"black",position:"absolute "}}>jdjdj</h1> 
+    </Container>
+</>
+    )
+}
+export default Header
